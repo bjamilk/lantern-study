ff30816e2fa49a85c88e842ab20eeb11179f91eb
bjamin <nimaj22@gmail.com>
2026-08-01 22:10:30 -0600

    Initial commit

---
+++ Lanternstudy/LanternstudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanternstudyApp.swift
+//  Lanternstudy
+//
+//  Created by Ben on 7/31/26.
+//
+
+import SwiftUI
+
+@main
+struct LanternstudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
